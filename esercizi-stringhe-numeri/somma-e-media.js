54e774201a5652daa7dacfbdd9fbda5c347bffcf
Ciro <207047069+XxCirolinoxX@users.noreply.github.com>
2025-11-10 15:25:36 +0100

    Calculate sum and average of five numbers

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,17 @@
   
   http://www.imparareaprogrammare.it
 */
+
+
+numerazione = 3, 7;
+let a = 3;
+let b = 4;
+let c = 5;
+let d = 6;
+let e = 7;
+
+let somma = a+b+c+d+e;
+let media = somma/5;
+
+console.log("somma = " + somma);
+console.log("media = " + media);
